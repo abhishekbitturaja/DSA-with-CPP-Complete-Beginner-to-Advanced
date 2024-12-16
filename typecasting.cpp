65391dc3d96d0745988ff7de292e686eef5f6475
Abhishek Kumar <abhishekbittu2021@gmail.com>
2024-12-16 20:39:55 +0530

    typecasting

---
+++ typecasting.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=98;
+    float b=32.25;
+    int c=(int)b;
+    cout<<c<<endl;
+    char ch='a';
+    cout<<(int)ch;
+
+    return 0;
+}
