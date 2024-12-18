5033d678206937230b3ca975b43e2d6b72b51602
Abhishek Kumar <abhishekbittu2021@gmail.com>
2024-12-18 09:20:39 +0530

    123 123 123

---
+++ number_pattern1.cpp
@@ -0,0 +1,33 @@
+
+
+
+// 123
+// 123
+// 123
+
+
+
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number : ";
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+        
+    }
+    
+    return 0;
+}
